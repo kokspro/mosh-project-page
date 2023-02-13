f2be8d873c372f9f62e46916757f80c1b9117f1b
kokspro <koks.programming@gmail.com>
2023-02-13 14:15:56 -0600

    .find() practice

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-const collapsibles = document.querySelectorAll('.collapsible');
-collapsibles.forEach((item) => 
-    item.addEventListener('click', function () {
-        this.classList.toggle('collapsible--expanded');
-    })
-);
+// const collapsibles = document.querySelectorAll('.collapsible');
+// collapsibles.forEach((item) => 
+//     item.addEventListener('click', function () {
+//         this.classList.toggle('collapsible--expanded');
+//     })
+// );
 
-const companies = [
-    {name: "Company One", category: "Finance", start: 1981, end: 2003},
-    {name: "Company Two", category: "Retail", start: 1992, end: 2008},
-    {name: "Company Three", category: "Auto", start: 1999, end: 2007},
-    {name: "Company Four", category: "Retail", start: 1989, end: 2010},
-    {name: "Company Five", category: "Technology", start: 2009, end: 2014},
-    {name: "Company Six", category: "Finance", start: 1987, end: 2010},
-    {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
-    {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
-    {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
-];
+// const companies = [
+//     {name: "Company One", category: "Finance", start: 1981, end: 2003},
+//     {name: "Company Two", category: "Retail", start: 1992, end: 2008},
+//     {name: "Company Three", category: "Auto", start: 1999, end: 2007},
+//     {name: "Company Four", category: "Retail", start: 1989, end: 2010},
+//     {name: "Company Five", category: "Technology", start: 2009, end: 2014},
+//     {name: "Company Six", category: "Finance", start: 1987, end: 2010},
+//     {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
+//     {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
+//     {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
+// ];
 
-const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+// const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
 
 
@@ -106,5 +106,21 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 // console.log(totalYears);
 
 
+//find
+
+const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const array2 = [1, 3, 5, 7, 9];
+
+// const matching = array1.filter(num => {
+//     return array2.find((number) => {
+//         return num === number;
+//     })
+// });
+// console.log(matching);
+
+const matching = array1.filter(num => array2[i] {
+    return num;
+}) 
+ 
 
 
